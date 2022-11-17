3c140779f244a6fa477a0b3d0afb9e43dba406fb
shivpalyadavv <shivpaly2@gmail.com>
2022-11-17 22:26:58 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv(r'C:\Users\shivpal\Documents\Seattle2014.csv')
+print(data.head())
+
+rainfall = data["PRCP"].values
+inches = rainfall/254
+print(inches.shape)
+import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
+plt.hist(inches, 40)
+plt.show()
+print("Number of days without rain: ", np.sum(inches == 0))
+print("Number of days with rain: ", np.sum(inches != 0))
+print("Number of days with rain more than 0.5 inches: ", np.sum(inches>0.5))
+print("Number of days with rain < 0.2 inches: ", np.sum((inches > 0)& (inches < 0.2)))
